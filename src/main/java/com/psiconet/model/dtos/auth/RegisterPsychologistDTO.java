087fc159b68f5dfeae8f50ff610095ed19e67965
jonathaneamorim <jonathaneamorim@gmail.com>
2026-03-11 00:12:48 -0300

    fix(register-flow): Criando DTOs para registro

---
+++ src/main/java/com/psiconet/model/dtos/auth/RegisterPsychologistDTO.java
@@ -0,0 +1,13 @@
+package com.psiconet.model.dtos.auth;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class RegisterPsychologistDTO {
+    private String email;
+    private String cpf;
+    private String crp;
+    private LocalDate birthDate;
+    private String password;
+}
