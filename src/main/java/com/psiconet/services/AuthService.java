145542d3fc676a7dcb7df9a2b2bd89e9b00e19e4
jonathaneamorim <jonathaneamorim@gmail.com>
2026-03-12 00:08:25 -0300

    feature(base-mappers): Ajustando Service de autenticação

--- src/main/java/com/psiconet/services/AuthService.java
+++ src/main/java/com/psiconet/services/AuthService.java
@@ -1,12 +1,17 @@
 package com.psiconet.services;
 
+import com.psiconet.exception.EmailAlreadyExistsException;
+import com.psiconet.mapper.PatientMapper;
+import com.psiconet.mapper.PsychologistMapper;
+import com.psiconet.mapper.UserMapper;
 import com.psiconet.model.dtos.auth.AuthenticationDTO;
-import com.psiconet.model.dtos.auth.RegisterPatientDTO;
-import com.psiconet.model.dtos.auth.RegisterPsychologistDTO;
+import com.psiconet.model.dtos.auth.PatientRegisterRequest;
+import com.psiconet.model.dtos.auth.PsychologistRegisterRequest;
 import com.psiconet.model.entities.access.User;
 import com.psiconet.model.entities.profile.Patient;
 import com.psiconet.model.entities.profile.Psychologist;
 import com.psiconet.model.enums.RoleEnum;
+import com.psiconet.model.enums.UserStatusEnum;
 import com.psiconet.repositories.profile.PatientRepository;
 import com.psiconet.repositories.profile.PsychologistRepository;
 import com.psiconet.repositories.access.UserRepository;
@@ -30,52 +35,40 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+    private final PatientMapper patientMapper;
+    private final PsychologistMapper psychologistMapper;
+    private final UserMapper userMapper;
 
     @Transactional
-    public void registerPatient(RegisterPatientDTO data) {
-        User user = createUser(data.getEmail(), data.getPassword(), RoleEnum.PACIENTE);
-
-        Patient patient = new Patient();
-        patient.setUser(user);
-        patient.setEmail(data.getEmail());
-        patient.setCpf(data.getCpf());
-        patient.setBirthDate(data.getBirthDate());
-        patient.setIsActive(true);
-
+    public void registerPatient(PatientRegisterRequest data) {
+        User user = createUser(data.getEmail(), data.getPassword(), RoleEnum.PATIENT);
+        userRepository.save(user);
+        Patient patient = patientMapper.toPatient(data, user);
         patientRepository.save(patient);
     }
 
     @Transactional
-    public void registerPsychologist(RegisterPsychologistDTO data) {
-        User user = createUser(data.getEmail(), data.getPassword(), RoleEnum.PSICOLOGO);
-
-        Psychologist psych = new Psychologist();
-        psych.setUser(user);
-        psych.setEmail(data.getEmail());
-        psych.setCpf(data.getCpf());
-        psych.setCrp(data.getCrp());
-        psych.setBirthDate(data.getBirthDate());
-        psych.setIsActive(true);
-
-        psychologistRepository.save(psych);
+    public void registerPsychologist(PsychologistRegisterRequest data) {
+        User user = createUser(data.getEmail(), data.getPassword(), RoleEnum.PSYCHOLOGIST);
+        userRepository.save(user);
+        Psychologist psychologist = psychologistMapper.toPsychologist(data, user);
+        psychologistRepository.save(psychologist);
     }
 
     private User createUser(String email, String password, RoleEnum role) {
-        if (userRepository.findByUsername(email) != null) {
-            throw new RuntimeException("E-mail já cadastrado!");
-        }
-        User user = new User();
-        user.setUsername(email); // O e-mail será o login
+        if (userRepository.existsByEmail(email)) throw new EmailAlreadyExistsException(email);
+        User user = userMapper.toUser(email, role);
         user.setPassword(passwordEncoder.encode(password));
-        user.setAccessRoleEnum(role);
-        return userRepository.save(user);
+        user.setStatus(UserStatusEnum.ACTIVE);
+        return user;
     }
 
     public String login(AuthenticationDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = authenticationManager.authenticate(usernamePassword);
+        User user = (User) auth.getPrincipal();
 
-        User user = (User) Objects.requireNonNull(auth.getPrincipal());
+        if (!UserStatusEnum.ACTIVE.equals(user.getStatus())) throw new IllegalStateException("Usuário não está ativo");
 
         return tokenService.generateToken(user);
     }
