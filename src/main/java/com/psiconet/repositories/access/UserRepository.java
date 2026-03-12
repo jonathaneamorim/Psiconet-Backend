b3a899986887e956d5fad49f1f7ac6cb3830a3c9
jonathaneamorim <jonathaneamorim@gmail.com>
2026-03-12 00:08:39 -0300

    feature(base-mappers): Ajustando Repository de usuario

--- src/main/java/com/psiconet/repositories/access/UserRepository.java
+++ src/main/java/com/psiconet/repositories/access/UserRepository.java
@@ -3,9 +3,10 @@ package com.psiconet.repositories.access;
 import com.psiconet.model.entities.access.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByUsername(String username);
-
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
