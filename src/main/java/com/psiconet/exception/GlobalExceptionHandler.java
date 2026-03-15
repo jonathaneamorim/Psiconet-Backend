7cc42da75aa807e16da442017c2171beded00303
jonathaneamorim <jonathaneamorim@gmail.com>
2026-03-15 16:19:20 -0300

    feature(register-validations): Inserindo handler de captura de exceção do validation

--- src/main/java/com/psiconet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/psiconet/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.psiconet.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 // Handler global que retorna um erro HTTP adequado dependendo da exception lançada
 /* Exemplo
@@ -17,5 +22,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<String> handleEmailExists(EmailAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    // Handler que captura exceções de validação de DTO e lança um bad request com os campos errados
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
