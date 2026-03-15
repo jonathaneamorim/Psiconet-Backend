38fd98d9ffe30bb536a625d57d8774468fb17747
jonathaneamorim <jonathaneamorim@gmail.com>
2026-03-14 22:02:22 -0300

    feature(base-user): Criando arquivo para inserir usuário admin base do sistema

--- src/main/java/com/psiconet/repositories/access/UserRepository.java
+++ src/main/java/com/psiconet/repositories/access/UserRepository.java
@@ -1,6 +1,7 @@
 package com.psiconet.repositories.access;
 
 import com.psiconet.model.entities.access.User;
+import com.psiconet.model.enums.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+    boolean existsByAccessRoleEnum(RoleEnum accessRoleEnum);
 }
